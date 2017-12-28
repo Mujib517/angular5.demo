07550e286a695fe3a228b08de8bfd1fe7e18abeb
Mujib517 <mujib517@gmail.com>
2017-12-28 09:36:49 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
+//Es 6 module
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector:'app-root',
+    template:'<h1>Hello World!!</h1>  <app-home></app-home>'
 })
 export class AppComponent {
-  title = 'app';
+
 }
