6b5ae18637e3f4ed0e754643806159008c355d6d
Mujib517 <mujib517@gmail.com>
2018-01-02 09:29:23 +0530

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,43 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     template: `
     
-    <h1>{{firstName}}</h1>
+    <h1 [ngClass]="cls">
+        {{firstName}}
+    </h1>
 
-  
+    <h1 [ngStyle]="styles">
+        {{lastName}}
+    </h1>
+
+    <table border="1">
+        <tr>
+            <td>Value</td>
+            <td>Index</td>
+            <td>Even</td>
+            <td>Odd</td>
+            <td>First</td>
+            <td>Last</td>
+
+        </tr>
+        <tr [ngClass]="evn?'fg':'fg2'" *ngFor="let val of arr; let idx=index;let evn=even;let odd=odd;let fst=first;let lst=last">
+            <td>{{val}}</td>
+            <td>{{idx}}</td>
+            <td>{{evn}}</td>
+            <td>{{odd}}</td>
+            <td>{{fst}}</td>
+            <td>{{lst}}</td>
+            
+        </tr>
+    </table>
+
+   
     <input type="text" [(ngModel)]="firstName"/>
 
 
     <h1 [style.border]="myProp1?'1px solid black':''" [class.fg]="myProp">{{getName()}}</h1>
     
     <button [hidden]="getValue()">Click</button>
-    <button (click)="onSave(1)">Save</button>
+    <button (click)="toggle()">Save</button>
 
     <select (change)="onChange()">
         <option>C</option>
@@ -22,12 +49,23 @@ import { Component } from '@angular/core';
         <option>C#</option>
     </select>
 
+    <div [ngSwitch]="priority">
+        <h1 *ngSwitchCase="'Low'">Low Priority</h1>
+        <h1 *ngSwitchCase="'Medium'">Medium Priority</h1>
+        <h1 *ngSwitchCase="'High'">High Priority</h1>
+        <h1 *ngSwitchDefault>Very Low Priority</h1>
+    </div>
      `
 })
 export class AppComponent {
-    myProp: string = "Angular App";
-    firstName: string = "";
+
+    myProp: boolean = true;
+    cls = { bg: true, fg: this.myProp };  //falsy truthy
+    firstName: string = "abcd";
     lastName: string = "md";
+    arr: number[] = [1, 2, 3, 4];
+    priority: string = "Very Low";
+    styles = { 'background-color': 'yellow','border':"3px solid gray" };
 
     getValue(): string {
         //complex
@@ -47,8 +85,28 @@ export class AppComponent {
         return this.firstName + " " + this.lastName;
     }
 
+    toggle() {
+        //var is function scoped
+        // if (true) {
+        //     let x = 10; //block scoped
+
+        //     if(true){
+        //         if(true){
+        //            var x='kajdf'
+        //         }
+        //     }
+        // }
+
+        // console.log(x);
+
+        this.myProp = !this.myProp;
+        //this.myProp = this.myProp ? false : true;
+        // if (this.myProp) this.myProp = false;
+        // else this.myProp = true;
+    }
+
     //truthy 
     //falsy  "",false,null,undefined,Nan,0
 }
-
+// ngIf, ngSwitch, ngModel, ngFor, ngClass, ngStyle
 // Expressions (interpolation), property binding, event binding , class, style and two way
