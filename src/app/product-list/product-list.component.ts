21e9a2e9b73060aba1a5b4052170c1318425116a
Mujib517 <mujib517@gmail.com>
2018-01-03 09:25:36 +0530

    Services

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ProductService } from "../shared/product.service";
 
 @Component({
     selector: 'app-product-list',
@@ -8,10 +9,8 @@ export class ProductListComponent {
     products: any[];
 
     constructor() {
-        this.products = [
-            { id: 1, brand: "Nokia", model: "N8", price: 200, inStock: true, lastUpdated: Date.now() },
-            { id: 2, brand: "Apple", model: "Iphone 8", price: 600, inStock: true, lastUpdated: Date.now() },
-            { id: 3, brand: "Samsung", model: "S8", price: 700, inStock: false, lastUpdated: Date.now() },
-            { id: 4, brand: "Sony", model: "Xperia Z", price: 800, inStock: false, lastUpdated: Date.now() }];
+        //hard to test
+        let productSvc=new ProductService();
+        this.products = productSvc.get();
     }
 }
---
+++ src/app/shared/product.service.ts
@@ -0,0 +1,11 @@
+
+
+export class ProductService{
+    get(){
+        return [
+            { id: 1, brand: "Nokia", model: "N8", price: 200, inStock: true, lastUpdated: Date.now() },
+            { id: 2, brand: "Apple", model: "Iphone 8", price: 600, inStock: true, lastUpdated: Date.now() },
+            { id: 3, brand: "Samsung", model: "S8", price: 700, inStock: false, lastUpdated: Date.now() },
+            { id: 4, brand: "Sony", model: "Xperia Z", price: 800, inStock: false, lastUpdated: Date.now() }];
+    }
+}
