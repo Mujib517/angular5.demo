dbe0dbd88a04a5d703a3a8a06bb0b0c6d0292506
Mujib517 <mujib517@gmail.com>
2018-01-08 09:41:45 +0530

    HTTP POST

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,13 +7,25 @@ import { ProductService } from "../shared/product.service";
 })
 export class ProductListComponent {
     products: any[];
+    product: any = {
+        brand: 'Nokia'
+    };
 
-    constructor(productSvc: ProductService) {
-
+    constructor(private productSvc: ProductService) {
         productSvc.get()
             .subscribe(
             res => this.products = res["data"],
             err => console.log(err)
             )
+    }
+
+    onSave() {
+        this.productSvc.save(this.product)
+            .subscribe(
+            () => console.log("Successfully saved"),
+            () => console.log("failed")
+            );
+
+        // this.productSvc.save();
     }
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import 'rxjs/add/operator/retry';
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ProductService {
     constructor(private http: HttpClient) { }
 
-    get() {
+    get(): Observable<any> {
         return this.http.get("https://exp-rest-api.herokuapp.com/api/products/")
-                .retry(3);
+            .retry(3);
+    }
+
+    save(product): Observable<any> {
+        return this.http.post("https://exp-rest-api.herokuapp.com/api/products/", product);
     }
 }
