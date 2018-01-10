6791980e5e9b54e26f70725df9aff1b9f9b5b29a
Mujib517 <mujib517@gmail.com>
2018-01-10 09:19:18 +0530

    Reactive Forms

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,25 +1,31 @@
 import { Component } from "@angular/core";
 import { ProductService } from "../shared/product.service";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.html'
 })
 export class ProductListComponent {
+    frm: FormGroup;
     products: any[];
     failed: boolean;
     success: boolean;
-    product: any = {
-        brand: 'Nokia'
-    };
 
-    constructor(private productSvc: ProductService) {
+    constructor(private productSvc: ProductService, private fb: FormBuilder) {
+        //LHS==RHS. Unit testing
+        this.frm = fb.group({
+            brand: ['Apple', [Validators.required]],
+            model: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(10)]],
+            price: ['',[Validators.pattern("[0-9]{3}")]],
+            inStock: []
+        });
+
         this.get();
     }
 
     // refactoring
     get() {
-        this.product = {};
         this.productSvc.get()
             .subscribe(
             res => this.products = res["data"],
@@ -28,14 +34,22 @@ export class ProductListComponent {
     }
 
     onSave() {
-        this.productSvc.save(this.product)
-            .subscribe(
-            () => {
-            this.success = true;
-                this.get()
-            },
-            () => this.failed = true
-            );
+        if (this.frm.valid) {
+            this.productSvc.save(this.frm.value)
+                .subscribe(
+                () => {
+                    this.success = true;
+                    this.frm.reset();
+                    this.get()
+                },
+                () => this.failed = true
+                );
+        }
+        else {
+            console.log("Validations failed");
+        }
+
+
 
         // this.productSvc.save();
     }
