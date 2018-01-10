b1d239e5a55225870b38230db6f8dee2f0802f34
Mujib517 <mujib517@gmail.com>
2018-01-10 09:30:46 +0530

    Refactoring

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,26 +1,15 @@
 import { Component } from "@angular/core";
 import { ProductService } from "../shared/product.service";
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.html'
 })
 export class ProductListComponent {
-    frm: FormGroup;
     products: any[];
-    failed: boolean;
-    success: boolean;
+   
 
-    constructor(private productSvc: ProductService, private fb: FormBuilder) {
-        //LHS==RHS. Unit testing
-        this.frm = fb.group({
-            brand: ['Apple', [Validators.required]],
-            model: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(10)]],
-            price: ['',[Validators.pattern("[0-9]{3}")]],
-            inStock: []
-        });
-
+    constructor(private productSvc: ProductService) {
         this.get();
     }
 
@@ -31,26 +20,5 @@ export class ProductListComponent {
             res => this.products = res["data"],
             err => console.log(err)
             )
-    }
-
-    onSave() {
-        if (this.frm.valid) {
-            this.productSvc.save(this.frm.value)
-                .subscribe(
-                () => {
-                    this.success = true;
-                    this.frm.reset();
-                    this.get()
-                },
-                () => this.failed = true
-                );
-        }
-        else {
-            console.log("Validations failed");
-        }
-
-
-
-        // this.productSvc.save();
     }
 }
