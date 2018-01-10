32ea09aa39ca95d249b31104231c1d487f06234c
Mujib517 <mujib517@gmail.com>
2018-01-10 09:40:49 +0530

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from './about/about.component';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductComponent } from './product/product.component';
-import { ProductService } from "./shared/product.service";
 import { UsersComponent } from './users/users.component';
-import { TimePipe } from './shared/time.pipe';
 import { NewProductComponent } from "./new-product/new-product.component";
+import { SharedModule } from "./shared/shared.module";
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule,SharedModule],
     declarations: [AppComponent, HomeComponent, AboutComponent,
-        ProductListComponent, ProductComponent, UsersComponent, TimePipe, NewProductComponent],
-    providers: [ProductService],
+        ProductListComponent, ProductComponent, UsersComponent, NewProductComponent],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
