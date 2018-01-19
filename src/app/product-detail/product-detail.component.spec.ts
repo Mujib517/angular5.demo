06a711e03db1f7400a3a099c5bfe4ab9f1354974
Mujib517 <mujib517@gmail.com>
2018-01-19 09:33:12 +0530

    Product detail tests

---
+++ src/app/product-detail/product-detail.component.spec.ts
@@ -0,0 +1,39 @@
+import { ProductDetailComponent } from './product-detail.component';
+import { Observable } from 'rxjs/Observable';
+
+
+fdescribe("Product Detail Component", function () {
+
+    let comp;
+    let productSvc;
+    let activatedRoute;
+
+    beforeEach(function () {
+
+        let obs = new Observable(o => o.next(10));
+
+        productSvc = jasmine.createSpyObj(["getById"])
+        productSvc.getById = jasmine.createSpy("getById").and.returnValue(obs);
+
+        activatedRoute = {
+            snapshot: {
+                params: {
+                    id: 10
+                }
+            }
+        };
+
+        comp = new ProductDetailComponent(productSvc, activatedRoute);
+
+    });
+
+    it('should have product defined', function () {
+        expect(comp.product).toBeDefined();
+    });
+
+    it('should ger prouct data by calling getById of product Svc', function () {
+        comp.ngOnInit();
+
+        expect(productSvc.getById).toHaveBeenCalledWith(10);
+    });
+});
